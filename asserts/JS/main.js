bc2b2cc91f5ed35acd3ec2a04bbb6a79ab94af8c
Navaharish <133334155+Navaharish@users.noreply.github.com>
2023-07-02 17:31:38 +0530

    debugging Tites and cross

--- asserts/JS/main.js
+++ asserts/JS/main.js
@@ -2,15 +2,15 @@ const navToggler = document.querySelector(".nav-toggler");
 const navLinks = document.querySelectorAll(".header-menu>li>a");
 const navLinkBtn = document.querySelector(".header-menu>li>.btn-primary");
 
-console.log(navLinkBtn);
 navToggler.addEventListener("click", changeMenuState);
 
 function changeMenuState() {
     const headerMenu = document.querySelector("ul.header-menu");
-    const navIcon = document.querySelectorAll(".navIcon");
+    const navIcon = document.querySelectorAll(".nav-icon");
 
     // Show menu programatically
     headerMenu.classList.toggle("slide");
+    console.log(navIcon)
 
     navIcon.forEach((icon) => {
         icon.classList.toggle("hidden");
@@ -57,7 +57,6 @@ function setMenuActive() {
 }
 
 function onMenuClick() {
-    console.log(navLinks);
 
     for (let i = 0; i < navLinks.length; i++) {
         navLinks[i].addEventListener("click", changeMenuState);
