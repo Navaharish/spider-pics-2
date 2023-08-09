1a7ebe808c80ff198c0f9f42293744e1c65e69c6
Navaharish <navaharish2005@gmail.com>
2023-08-09 13:48:32 +0530

    pls commit

--- asserts/JS/main.js
+++ asserts/JS/main.js
@@ -5,70 +5,26 @@ const navLinkBtn = document.querySelector(".header-menu>li>.btn-primary");
 navToggler.addEventListener("click", changeMenuState);
 
 function changeMenuState() {
+    const dropDown = document.querySelector('.dropdown-toggle')
     const headerMenu = document.querySelector("ul.header-menu");
     const navIcon = document.querySelectorAll(".nav-icon");
 
     // Show menu programatically
     headerMenu.classList.toggle("slide");
-
+    // dropDown.classList.toggle('dd-slide')
     navIcon.forEach((icon) => {
         icon.classList.toggle("hidden");
     });
 }
 
-function navbarFixed() {
-    const header_dom = document.querySelector(".header");
-    const nav_offset_top = header_dom.clientHeight + 50;
 
-    window.addEventListener("scroll", () => {
-        let scroll = window.pageYOffset || document.documentElement.scrollTop;
 
-        if (scroll >= nav_offset_top) {
-            header_dom.classList.add("navbar-fixed");
-        } else {
-            header_dom.classList.remove("navbar-fixed");
-        }
-    });
-}
-
-function setMenuActive() {
-    const sections = document.querySelectorAll("section");
-
-    window.addEventListener("scroll", () => {
-        let current = "";
-        sections.forEach((section) => {
-            const sectionTop = section.offsetTop;
-            const sectionHeight = section.clientHeight;
-
-            if (window.pageYOffset >= sectionTop - sectionHeight / 3) {
-                current = section.getAttribute("id");
-            }
-        });
-
-        navLinks.forEach((li) => {
-            li.classList.remove("Active");
-
-            if (current == li.getAttribute("href").split("#")[1]) {
-                li.classList.add("Active");
-            }
-        });
-    });
-}
-
-function onMenuClick() {
-
-    for (let i = 0; i < navLinks.length; i++) {
-        navLinks[i].addEventListener("click", changeMenuState);
-    };
-
-}
 
 function onMenuClickBtn() {
 
     navLinkBtn.addEventListener("click", changeMenuState);
 }
 
-navbarFixed();
-setMenuActive();
+
 onMenuClick();
 onMenuClickBtn();
