daea353a09d14506dfbadb420595253a36c635c2
Navaharish <navaharish2005@gmail.com>
2023-08-09 18:38:44 +0530

    redirecting problem is rectified

--- asserts/JS/main.js
+++ asserts/JS/main.js
@@ -5,7 +5,6 @@ const navLinkBtn = document.querySelector(".header-menu>li>.btn-primary");
 navToggler.addEventListener("click", changeMenuState);
 
 function changeMenuState() {
-    const dropDown = document.querySelector('.dropdown-toggle')
     const headerMenu = document.querySelector("ul.header-menu");
     const navIcon = document.querySelectorAll(".nav-icon");
 
@@ -14,30 +13,6 @@ function changeMenuState() {
     // dropDown.classList.toggle('dd-slide')
     navIcon.forEach((icon) => {
         icon.classList.toggle("hidden");
-    });
-}
-
-function setMenuActive() {
-    const sections = document.querySelectorAll("section");
-
-    window.addEventListener("scroll", () => {
-        let current = "";
-        sections.forEach((section) => {
-            const sectionTop = section.offsetTop;
-            const sectionHeight = section.clientHeight;
-
-            if (window.pageYOffset >= sectionTop - sectionHeight / 3) {
-                current = section.getAttribute("id");
-            }
-        });
-
-        navLinks.forEach((li) => {
-            li.classList.remove("active");
-
-            if (current == li.getAttribute("href").split("#")[1]) {
-                li.classList.add("active");
-            }
-        });
     });
 }
 
@@ -58,4 +33,3 @@ function onMenuClickBtn() {
 
 onMenuClick();
 onMenuClickBtn();
-setMenuActive();
